a07a25b7499d8aa3901d7554a2beed6ad6ec7c07
Becky449 <82038825+Becky449@users.noreply.github.com>
2022-10-28 15:16:26 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import './style.css';
 import { renderContainer, toAddScore } from './modules/data.js';
 
-let gameIdentity = '';
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-
-  method: 'POST',
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-  body: JSON.stringify({
-    name: 'Handball tx',
-  }),
-}).then((res) => res.json())
-  .then((gameID) => {
-    const response = gameID.result;
-    gameIdentity = response.substring(14, response.lastIndexOf(' '));
-  });
-
 const refresh = document.querySelector('.r-btn');
 
 refresh.addEventListener('click', () => {
-  renderContainer(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zilCJ92DyR9p0QhPQKZI/scores`);
+  renderContainer('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zilCJ92DyR9p0QhPQKZI/scores');
 });
 
 const submitscore = document.getElementById('submit');
@@ -30,7 +14,7 @@ submitscore.addEventListener('click', (e) => {
   e.preventDefault();
   if (yourName.value !== '' && score.value !== '') {
     submitscore.classList.add('onfocuss');
-    toAddScore(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zilCJ92DyR9p0QhPQKZI/scores`, yourName.value, score.value);
+    toAddScore('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zilCJ92DyR9p0QhPQKZI/scores', yourName.value, score.value);
     document.forms[0].reset();
   }
 });
